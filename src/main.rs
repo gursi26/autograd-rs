47a04c934e1d1b8191b8b28e296ee8a1824a6704
gursi26 <gursimar.batra@gmail.com>
2023-06-17 20:53:07 +0530

    restarted again

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-#![allow(unused_mut)]
-#![allow(dead_code)]
-
-mod tensor;
-mod dcg;
-
 fn main() {
-    let val = tensor::Value::new(100.0);
-    println!("{}", val);
+    println!("Hello world!");
 }
