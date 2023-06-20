f23938f027b643f2f6109a0ef107dabd7b65bf45
gursi26 <gursimar.batra@gmail.com>
2023-06-20 20:26:29 +0530

    FINALLY WORKS YAYAYAYYAYAYYAYAYAY

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,20 @@
 
 use std::println;
 
+use wrapper::Operable;
+
 mod tensor;
 mod graph;
 mod ops;
 mod compute;
 mod wrapper;
 
+use wrapper::*;
+
 fn main() {
-    let mut t1 = parameter![1, 2, 3, 4, 5];
-    let mut t2 = parameter![2, 2, 2, 2, 2];
-    {
-        let mut subtract_node = graph::Node::BinaryOpNode {
-            rhs: Box::new(graph::Node::TensorNode { tensor: t1.clone(), source: &mut t1 }),
-            lhs: Box::new(graph::Node::UnaryOpNode {
-                value: Box::new(graph::Node::TensorNode { tensor: t2.clone(), source: &mut t2 }),
-                op: ops::UnaryOp::Negate
-            }),
-            op: ops::BinaryOp::Add
-        };
-        let (mut grad_ptrs, mut grad_values, output) = subtract_node.eval();
-        for (ptr, grad) in grad_ptrs.drain(..).zip(grad_values.drain(..)) {
-            ptr.grad = Some(grad);
-        }
-        println!("Output: {:?}", output);
-    }
-    println!("T1: {:?}\nT2: {:?}", t1, t2);
+    let mut t = parameter![-0.1, 0.2, 0.8, 0.9, 3.14];
+    let mut cnst = tensor![1, 1, 1, 1, 1];
+    let sigmoid = reciprocal(add(&mut cnst, exp(negate(&mut t))));
+    let sigmoid_output = eval(sigmoid);
+    println!("Parameter: {:?}\nOutput: {:?}", t, sigmoid_output);
 }
---
+++ src/wrapper.rs
@@ -0,0 +1,90 @@
+use crate::tensor::Tensor;
+use crate::graph::Node;
+use crate::ops::{BinaryOp, UnaryOp};
+
+pub trait Operable<'a> {
+    fn make_operable(self) -> Node<'a>;
+}
+
+impl<'a> Operable<'a> for Node<'a> {
+    fn make_operable(self) -> Node<'a> {
+        self
+    }
+}
+
+impl <'a> Operable<'a> for &'a mut Tensor {
+    fn make_operable(self: &'a mut Tensor) -> Node<'a> {
+        Node::TensorNode { tensor: self.clone(), source: self }
+    }
+}
+
+pub fn eval<'a>(mut root_node: Node<'a>) -> Tensor {
+    let (mut grad_ptrs, mut grad_values, output_tensor) = root_node.eval();
+    for (ptr, grad) in grad_ptrs.drain(..).zip(grad_values.drain(..)) {
+        ptr.grad = Some(grad);
+    }
+    output_tensor.clone()
+}
+
+pub fn add<'a, T, U>(lhs: T, rhs: U) -> Node<'a>
+where
+    T: Operable<'a>,
+    U: Operable<'a>
+{
+    let (lhs, rhs) = (lhs.make_operable(), rhs.make_operable());
+    Node::BinaryOpNode {
+        lhs: Box::new(lhs),
+        rhs: Box::new(rhs),
+        op: BinaryOp::Add
+    }
+}
+
+pub fn mul<'a, T, U>(lhs: T, rhs: U) -> Node<'a>
+where
+    T: Operable<'a>,
+    U: Operable<'a>
+{
+    let (lhs, rhs) = (lhs.make_operable(), rhs.make_operable());
+    Node::BinaryOpNode {
+        lhs: Box::new(lhs),
+        rhs: Box::new(rhs),
+        op: BinaryOp::Multiply
+    }
+}
+
+pub fn pow<'a, T, U>(lhs: T, rhs: U) -> Node<'a>
+where
+    T: Operable<'a>,
+    U: Operable<'a>
+{
+    let (lhs, rhs) = (lhs.make_operable(), rhs.make_operable());
+    Node::BinaryOpNode {
+        lhs: Box::new(lhs),
+        rhs: Box::new(rhs),
+        op: BinaryOp::Pow
+    }
+}
+
+pub fn negate<'a, T: Operable<'a>>(value: T) -> Node<'a> {
+    let value = value.make_operable();
+    Node::UnaryOpNode {
+        value: Box::new(value),
+        op: UnaryOp::Negate
+    }
+}
+
+pub fn exp<'a, T: Operable<'a>>(value: T) -> Node<'a> {
+    let value = value.make_operable();
+    Node::UnaryOpNode {
+        value: Box::new(value),
+        op: UnaryOp::Exp
+    }
+}
+
+pub fn reciprocal<'a, T: Operable<'a>>(value: T) -> Node<'a> {
+    let value = value.make_operable();
+    Node::UnaryOpNode {
+        value: Box::new(value),
+        op: UnaryOp::Reciprocal
+    }
+}
